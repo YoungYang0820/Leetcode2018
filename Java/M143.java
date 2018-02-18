b72a758b9ba3a267021e0bdc63e31fc2833b253c
young <youngyang0820@gmail.com>
2018-02-18 08:57:53 -0800

    2018.2.18 143(Medium), 234(Easy)

---
+++ Java/M143.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+        ListNode fast = head, slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow.next = reverse(slow.next);
+        ListNode mid = slow;
+        slow = head;
+        fast = mid.next;
+        while (slow != mid) {
+            mid.next = fast.next;
+            fast.next = slow.next;
+            slow.next = fast;
+            slow = fast.next;
+            fast = mid.next;
+        }
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
