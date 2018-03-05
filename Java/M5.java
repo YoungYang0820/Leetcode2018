71086874737ef207091b07703e49cf0d25dbab6a
young <youngyang0820@gmail.com>
2018-03-04 19:55:20 -0800

    2018.3.4 5(Medium) 169(Easy)

---
+++ Java/M5.java
@@ -0,0 +1,23 @@
+class Solution {
+    private int start, max;
+    public String longestPalindrome(String s) {
+        if (s.length() < 2) return s;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i + 1);
+        }
+        return s.substring(start, start + max);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (max < k - j - 1) {
+            start = j + 1;
+            max = k - j - 1;
+        }
+    }
+}
